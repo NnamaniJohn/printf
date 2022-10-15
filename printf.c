1884b1be8013d91b46b52223b7ad3b332389cd5a
Nnamani John <nnamanijoh123@gmail.com>
2022-10-15 23:21:12 +0000

    printf function created

---
+++ printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - does formatted printing of text
+ * @format: string use to format text
+ * Return: number of characters
+ */
+
+int _printf(const char *format, ...)
+{
+    va_list arg;
+    int i, j, len, count;
+    int get_format = 0;
+    format_t f_t[] = {
+        {'c', print_char},
+        {'s', print_string},
+        {'%', print_percent},
+        {'d', print_integer},
+        {NULL, NULL}
+    };
+
+    len = count = 0;
+    while(format[len])
+        len++;
+
+    va_start(arg, format);
+    for(i = 0; i < len; i++)
+    {
+        if (get_format)
+        {
+            for (j = 0; format[j].format; j++)
+            {
+                if (format[i] == f_t[j].format)
+                    count += f_t[j].func(arg);
+            }
+            get_format = 0;
+        }
+        else
+        {
+            if (format[i] == '%')
+                get_format = 1;
+            else
+                count += _putchar(format[i]);
+        }
+    }
+    va_end(arg);
+
+    return (count);
+}
