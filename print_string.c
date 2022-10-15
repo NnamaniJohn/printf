58a9e60c03d117c696b5fa905c9a6339e807a6ae
Nnamani John <nnamanijoh123@gmail.com>
2022-10-15 23:38:11 +0000

    add function to print string

---
+++ print_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_string - prints string to stdout
+ * @s: string to print
+ * Return: count of character
+ */
+
+int print_string(va_list s)
+{
+	int i, count, len;
+	char *str = va_arg(s, char *);
+	
+	count = len = 0;
+	while (str[len])
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		count += _putchar(str[i]);
+	}
+	return (count);
+}
+
