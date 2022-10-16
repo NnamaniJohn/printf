57c5a18730e7b1709ff73a0457dbfe0b35d80e28
Nnamani John <nnamanijoh123@gmail.com>
2022-10-16 03:06:49 +0000

    u, o, x, X modifiers

--- printf.c
+++ printf.c
@@ -2,6 +2,38 @@
 #include <stddef.h>
 
 /**
+ * get_ft - get format
+ * @format: format
+ * @arg: arg
+ * Return: count
+ */
+
+int get_ft(char format, va_list arg)
+{
+	int j, is_format = 0, count = 0;
+	format_t f_t[] = {{'c', print_char}, {'s', print_string}, {'%', print_perc},
+		{'d', print_int}, {'i', print_int}, {'b', print_bin}, {'u', print_uint},
+		{'o', print_oct}, {'x', print_shex}, {'X', print_hex}, {'\0', NULL}
+	};
+
+	for (j = 0; f_t[j].format; j++)
+	{
+		if (format == f_t[j].format)
+		{
+			count += f_t[j].func(arg);
+			is_format = 1;
+			break;
+		}
+	}
+	if (!is_format)
+	{
+		count += _putchar('%');
+		count += _putchar(format);
+	}
+	return (count);
+}
+
+/**
  * _printf - does formatted printing of text
  * @format: string use to format text
  * Return: number of characters
@@ -10,13 +42,13 @@
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int i, j, len = 0, count = 0;
-	int get_format = 0, is_format = 0;
-	format_t f_t[] = {{'c', print_char}, {'s', print_string}, {'%', print_perc},
-		{'d', print_int}, {'i', print_int}, {'b', print_bin}, {'u', print_uint},
-		{'o', print_oct}, {'x', print_shex}, {'X', print_hex}, {'\0', NULL}
-	};
-
+	int i, len = 0, count = 0;
+	int get_format = 0;
+	
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (format[len])
 		len++;
 	va_start(arg, format);
@@ -24,20 +56,7 @@ int _printf(const char *format, ...)
 	{
 		if (get_format)
 		{
-			for (j = 0; f_t[j].format; j++)
-			{
-				if (format[i] == f_t[j].format)
-				{
-					count += f_t[j].func(arg);
-					is_format = 1;
-					break;
-				}
-			}
-			if (!is_format)
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
+			count += get_ft(format[i], arg);
 			get_format = 0;
 		}
 		else
