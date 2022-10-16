6a9a5a58243af7f7f031d2fef4417d54555aeaa5
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-16 16:59:35 +0100

    print_shex modified

--- print_shex.c
+++ print_shex.c
@@ -8,25 +8,25 @@
 
 int prt_shex(unsigned int num)
 {
-	int c = 0;
+	int count = 0;
 
 	if ((num / 16) > 0)
-		c += prt_shex(num / 16);
+		count += prt_shex(num / 16);
 	if (num % 16 == 10)
-		c += _putchar('a');
+		count += _putchar('a');
 	else if (num % 16 == 11)
-		c += _putchar('b');
+		count += _putchar('b');
 	else if (num % 16 == 12)
-		c += _putchar('c');
+		count += _putchar('c');
 	else if (num % 16 == 13)
-		c += _putchar('d');
+		count += _putchar('d');
 	else if (num % 16 == 14)
-		c += _putchar('e');
+		count += _putchar('e');
 	else if (num % 16 == 15)
-		c += _putchar('f');
+		count += _putchar('f');
 	else
-		c += _putchar((num % 16) + '0');
-	return (c);
+		count += _putchar((num % 16) + '0');
+	return (count);
 }
 
 /**
