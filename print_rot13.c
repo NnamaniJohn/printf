7aeb43ba52b8773e7c287d942f54a482dcc6410c
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-17 15:35:15 +0100

    R modifier

--- print_rot13.c
+++ print_rot13.c
@@ -10,6 +10,7 @@ int print_rot13(va_list r)
 {
 	unsigned int i, j, count;
 	char *str;
+	int printed;
 	char prev_char[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char hash_char[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
@@ -17,15 +18,18 @@ int print_rot13(va_list r)
 
 	for (i = 0; str[i]; i++)
 	{
+		printed = 0;
 		for (j = 0; prev_char[j]; j++)
 		{
 			if (str[i] == prev_char[j])
 			{
 				count += _putchar(hash_char[j]);
+				printed = 1;
 				break;
 			}
+
 		}
-		if (str[i] < 'A' || (str[i] > 'Z' && str[i] < 'a') || str[j] > 'z')
+		if (!prev_char[j] && printed == 0)
 			count += _putchar(str[i]);
 	}
 	return (count);
