b42bf2c1958c83ea9cb3dc047ae37f6c9add3089
David Egwuatu <davidegwuatu2@gmail.com>
2022-10-17 17:16:09 +0100

    upate

--- print_rot13.c
+++ print_rot13.c
@@ -8,33 +8,11 @@
 
 int print_rot13(va_list r)
 {
-	unsigned int i, j, count;
+	int i, count;
 	char *str;
-	/*int printed;*/
-	/*char prev_char[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char hash_char[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";*/
-	char *prev_char, *hash_char;
 	str = va_arg(r, char *);
 
 
-	/*for (i = 0; str[i]; i++)
-	{
-		printed = 0;
-		for (j = 0; prev_char[j]; j++)
-		{
-			if (str[i] == prev_char[j])
-			{
-				count += _putchar(hash_char[j]);
-				printed = 1;
-				break;
-			}
-			else if (str[i] != prev_char[j] && (printed == 0 || !prev_char[j]))
-				count += _putchar(str[i]);
-
-		}
-		//if (printed == 0 || !prev_char[j])
-		//	count += _putchar(str[i]);
-	}*/
 	for (i = 0; str[i]; i++)
 	{
 		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
