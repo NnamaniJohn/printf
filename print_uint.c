b681f83f58337759bab3225b132f3fa00bad06fb
Nnamani John <nnamanijoh123@gmail.com>
2022-10-18 07:45:39 +0000

    fix long int

--- print_uint.c
+++ print_uint.c
@@ -27,7 +27,7 @@ int print_ulnum(unsigned long int num)
 	int count = 0;
 
 	if ((num / 10) > 0)
-		count += print_unum(num / 10);
+		count += print_ulnum(num / 10);
 	count += _putchar((num % 10) + '0');
 	return (count);
 }
@@ -43,7 +43,7 @@ int print_uhnum(unsigned short int num)
 	int count = 0;
 
 	if ((num / 10) > 0)
-		count += print_unum(num / 10);
+		count += print_uhnum(num / 10);
 	count += _putchar((num % 10) + '0');
 	return (count);
 }
@@ -57,7 +57,7 @@ int print_uhnum(unsigned short int num)
 int print_uint(va_list num, flag_t *flag)
 {
 	if (flag->lon)
-		return (print_ulnum(va_arg(num, unsigned int)));
+		return (print_ulnum(va_arg(num, unsigned long int)));
 	else if (flag->sht)
 		return (print_uhnum(va_arg(num, unsigned int)));
 	else
